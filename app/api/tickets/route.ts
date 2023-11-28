af390dfa575d099f0b49e48a716300d306737524
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-11-29 02:35:31 +0330

    create tickets api route, POST and GET calls

---
+++ app/api/tickets/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createTicketSchema = z.object({
+  title: z.string().min(3).max(255),
+  description: z.string().min(3),
+});
+
+export async function GET() {
+  const tickets = await prisma.ticket.findMany();
+
+  return NextResponse.json(tickets, { status: 200 });
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createTicketSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const newTicket = await prisma.ticket.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newTicket, { status: 201 });
+}
