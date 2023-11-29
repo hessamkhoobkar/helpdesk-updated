6376041a5535b4cdab70836797c05cded91b46e0
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-11-29 04:02:08 +0330

    handle form submission

--- app/tickets/new/page.tsx
+++ app/tickets/new/page.tsx
@@ -1,20 +1,43 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
+import { Button, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
 const SimpleMdeEditor = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
-import "easymde/dist/easymde.min.css";
+
+interface TicketForm {
+  title: string;
+  description: string;
+}
 
 export default function NewTicketPage() {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<TicketForm>();
+
   return (
-    <div className="space-y-3">
+    <form
+      className="space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/tickets", data);
+        router.push("/tickets");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMdeEditor />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMdeEditor placeholder="Description" {...field} ref={null} />
+        )}
+      />
       <Button>Create new ticket</Button>
-    </div>
+    </form>
   );
 }
